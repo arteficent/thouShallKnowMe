9849c0a7a32a679fc499ce3cee675e1d12ad1b1a
arteficent <sourabh9312324@gmail.com>
2021-06-30 17:21:32 +0530

    main

--- src/components/angelicTheme/angelicTheme.js
+++ src/components/angelicTheme/angelicTheme.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './angelicTheme.css'
-import {ReactComponent as Sun} from './angelicIcons/sun.svg';
-import {ReactComponent as Cloud1} from './angelicIcons/cloud1.svg';
-import {IoSunny} from 'react-icons/io';
+import { ReactComponent as Sun } from './angelicIcons/sun.svg';
+import { ReactComponent as Cloud1 } from './angelicIcons/cloud1.svg';
 
 function AngelicTheme() {
-   return (
-       <div className='dpShadow'>
-           <IoSunny className='sun-logo'/>
-           <Cloud1 className='cloud'/>
-       </div>             
+
+    return (
+        <div className='dpShadow'>
+            <Sun className='sun-logo' />
+            {/* <Cloud1 className='cloud' /> */}
+        </div>
     )
 }
 
