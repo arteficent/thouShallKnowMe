da2a078cc5b5e9cf75b294c02f2be73053872ec9
arteficent <sourabh9312324@gmail.com>
2021-07-01 14:29:22 +0530

    main

---
+++ src/components/pages/hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+function Hero() {
+    return (
+        <div>
+            <Card>
+                
+            </Card>
+        </div>
+    );
+}
+
+export default Hero;
